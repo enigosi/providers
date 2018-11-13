3e37b5047046e2056be9d57051cfa9821e7baa62
Wojciech Romanski <enigosi@gmail.com>
2018-11-13 12:14:19 +0100

    enable CORS

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,12 @@
 import * as express from 'express';
+import * as cors from 'cors';
 import handler from './handler';
 
 // Set up the express app
 const app = express();
+
+// enable CORS
+app.use(cors());
 
 app.get('/providers', handler);
 const PORT = 5000;
